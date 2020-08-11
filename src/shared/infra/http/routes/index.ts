1bd73f467f1fed3034592a92742752f5c6432954
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-11 19:43:23 -0300

    Add password routes and create table for tokens

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const router = Router();
 
 router.use('/appointments', appointmentsRouter);
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
+router.use('/password', passwordRouter);
 
 export default router;

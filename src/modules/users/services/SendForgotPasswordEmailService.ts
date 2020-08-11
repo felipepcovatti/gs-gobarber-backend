8a9fd3d0efa7d4834d3b1d0e343972b752f2109b
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-11 13:59:58 -0300

    Set basic service and tests for password recovery

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,34 @@
+import { injectable, inject } from 'tsyringe';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;

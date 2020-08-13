7ee9c8b0da85742a4d57a4fe4fffb953e67c6060
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-13 17:46:05 -0300

    Add service to know when a provider is available

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,55 @@
+import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  async execute({ provider_id, month, year }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      { provider_id, month, year },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      {
+        length: numberOfDaysInMonth,
+      },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const totalOfAppointmentsInDay = appointments.reduce(
+        (total, appointment) => {
+          return +(getDate(appointment.date) === day) + total;
+        },
+        0,
+      );
+
+      return {
+        day,
+        available: totalOfAppointmentsInDay < 10,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderMonthAvailabilityService;

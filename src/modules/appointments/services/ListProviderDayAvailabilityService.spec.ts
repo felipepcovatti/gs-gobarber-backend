52161b8806483adb2556f2ead1fc0bd3e3b08ed2
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-14 07:49:42 -0300

    Add test for service related to hours available

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,44 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the day availability from a provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      date: new Date(2020, 3, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      date: new Date(2020, 3, 20, 10, 0, 0),
+    });
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id: 'provider-id',
+      day: 20,
+      month: 4,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: true },
+        { hour: 10, available: false },
+        { hour: 11, available: true },
+      ]),
+    );
+  });
+});

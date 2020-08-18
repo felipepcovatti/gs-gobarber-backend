0d0d4a475ada9316a4d33c9dd7d0c1a7d238a8cd
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-18 17:09:00 -0300

    Add aws mail provider and modularize providers

---
+++ src/config/mail.ts
@@ -0,0 +1,23 @@
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'equipe@gobarber.com.br',
+      name: 'Equipe GoBarber',
+    },
+  },
+} as IMailConfig;
